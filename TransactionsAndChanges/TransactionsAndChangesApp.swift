9d88ab82f581eeb9585d98281231b10574a094a6
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2022-09-27 18:25:56 +0200

    Initial Commit

---
+++ TransactionsAndChanges/TransactionsAndChangesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionsAndChangesApp.swift
+//  TransactionsAndChanges
+//
+//  Created by Jose Manuel Ortiz Sanchez on 27/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TransactionsAndChangesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
