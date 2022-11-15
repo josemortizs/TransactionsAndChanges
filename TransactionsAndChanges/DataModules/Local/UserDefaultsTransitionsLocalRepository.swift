48f517185a3b42f6460f68ab923de8e0af397b76
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2022-11-15 22:44:11 +0100

    Create UserDefaultsTransitionsLocalRepository.swift

---
+++ TransactionsAndChanges/DataModules/Local/UserDefaultsTransitionsLocalRepository.swift
@@ -0,0 +1,60 @@
+//
+//  UserDefaultsTransitionsLocalRepository.swift
+//  TransactionsAndChanges
+//
+//  Created by Jose Manuel Ortiz Sanchez on 29/9/22.
+//
+
+import Foundation
+
+final class UserDefaultsTransitionsLocalRepository: TransactionsLocalRepositoryProtocol {
+    
+    func saveChanges(changes: [Change]) {
+        
+        let jsonEncoder = JSONEncoder()
+        let userDefault = UserDefaults.standard
+
+        do {
+            let jsonData = try jsonEncoder.encode(changes)
+            let json = String(data: jsonData, encoding: String.Encoding.utf8)
+            
+            userDefault.setValue(json, forKey: "changes")
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    func getChanges() -> [Change]? {
+        
+        let userDefault = UserDefaults.standard
+        let json = userDefault.string(forKey: "changes") ?? ""
+        
+        return StringDecoder.decode(model: Change.self, stringData: json)
+
+    }
+    
+    func saveTransactions(transactions: [Transaction]) {
+        
+        let jsonEncoder = JSONEncoder()
+        let userDefault = UserDefaults.standard
+
+        do {
+            let jsonData = try jsonEncoder.encode(transactions)
+            let json = String(data: jsonData, encoding: String.Encoding.utf8)
+            
+            userDefault.setValue(json, forKey: "transactions")
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    func getTransactions() -> [Transaction]? {
+        
+        let userDefault = UserDefaults.standard
+        let json = userDefault.string(forKey: "transactions") ?? ""
+        
+        return StringDecoder.decode(model: Transaction.self, stringData: json)
+    }
+}
